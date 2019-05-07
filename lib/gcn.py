add7703571355f7b3c6f0a52ae11f978a2d2a5c2
bob-chou <bobchou2016@gmail.com>
2019-05-07 22:22:16 +0800

    support basic st-gcn model

--- lib/gcn.py
+++ lib/gcn.py
@@ -34,12 +34,20 @@ def check_adjacent(A):
 
 class GraphConv(tf.keras.layers.Layer):
     """Basic graphic convolutional layer for keras"""
-    def __init__(self, filters, t_kernels=1, t_strides=1, **kwargs):
+    def __init__(self, filters, kernels=1, strides=1, **kwargs):
+        """First conv timesteps with [kernels, strides] and
+           conv graphically on intra-body edges according to given A
+
+        :param filters: number of output channels
+        :param kernels: convolutional kernels on timesteps
+        :param strides: convolutional strides on timesteps
+        :param kwargs: other args to feed in Conv2D on timesteps
+        """
         super(GraphConv, self).__init__()
         self.filters = filters
-        self.t_kernels = t_kernels // 2 * 2 + 1
+        self.t_kernels = kernels // 2 * 2 + 1
         kwargs["padding"] = "same"
-        kwargs["strides"] = (t_strides, 1)
+        kwargs["strides"] = (strides, 1)
         self.kwargs = kwargs
         self.Conv2D = None
         self.Reshape = None
@@ -59,12 +67,12 @@ class GraphConv(tf.keras.layers.Layer):
                              "[label_number, nodes, nodes], but find "
                              "{} of {}".format(len(A_shape), A_shape))
         _, T, N, C = x_shape
-
-        self.Conv2D = tf.keras.layers.Conv2D(self.filters * A_shape[0],
+        self.Conv2D = tf.keras.layers.Conv2D(self.filters * A_shape[0].value,
                                              (self.t_kernels, 1),
                                              **self.kwargs)
-        self.Reshape = tf.keras.layers.Reshape((-1, T, N, A_shape[0],
-                                                C//A_shape[0]))
+        self.Reshape = tf.keras.layers.Reshape((T.value, N.value,
+                                                A_shape[0].value,
+                                                C.value//A_shape[0].value))
         super(GraphConv, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
@@ -87,15 +95,25 @@ class GraphConv(tf.keras.layers.Layer):
             [self.filters]), A_shape]
 
 
-class TemporalConv(tf.keras.layers.Layer):
+class TempoConv(tf.keras.layers.Layer):
     """Basic graphic convolutional layer for keras"""
-    def __init__(self, filters=-1, t_kernels=3, t_strides=1,
-                 in_batchnorm=True, out_batchnorm=True, dropout=0, **kwargs):
-        super(TemporalConv, self).__init__()
+    def __init__(self, filters=-1, kernels=3, strides=1,
+                 in_batchnorm=True, out_batchnorm=True, dropout=0., **kwargs):
+        """
+
+        :param filters: number of output channels
+        :param kernels: convolutional kernels on timesteps
+        :param strides: convolutional strides on timesteps
+        :param in_batchnorm: whether to apply batch normalization on input
+        :param out_batchnorm: whether to apply batch normalization on output
+        :param dropout: whether to apply dropout on output
+        :param kwargs: other args to feed in Conv2D on timesteps
+        """
+        super(TempoConv, self).__init__()
         self.filters = filters
-        self.t_kernels = t_kernels // 2 * 2 + 1
+        self.t_kernels = kernels // 2 * 2 + 1
         kwargs["padding"] = "same"
-        kwargs["strides"] = (t_strides, 1)
+        kwargs["strides"] = (strides, 1)
         self.kwargs = kwargs
 
         if in_batchnorm:
@@ -106,21 +124,22 @@ class TemporalConv(tf.keras.layers.Layer):
             self.BatchNorm1 = tf.keras.layers.BatchNormalization(axis=-1)
         else:
             self.BatchNorm1 = tf.keras.layers.Lambda(lambda x: x)
-        self.Dropout = tf.keras.layers.Dropout(dropout)
+        self.Dropout = tf.keras.layers.Dropout(rate=dropout)
         self.Conv2D = None
 
     def build(self, input_shape):
-        if len(input_shape) != 3:
+        if len(input_shape) != 4:
             raise ValueError("Input shape should be 3 dimension in "
                              "[timesteps, nodes, channels], but find "
-                             "{} of {}".format(len(input_shape), input_shape))
+                             "{} of {}".format(len(input_shape),
+                                               input_shape[1:]))
         if self.filters < 0:
             self.filters = input_shape[-1]
         self.Conv2D = tf.keras.layers.Conv2D(self.filters,
                                              (self.t_kernels, 1),
-                                             activation=tf.keras.layers.ReLU,
+                                             activation=tf.keras.layers.ReLU(),
                                              **self.kwargs)
-        super(TemporalConv, self).build(input_shape)
+        super(TempoConv, self).build(input_shape)
 
     def call(self, inputs, **kwargs):
         inputs = self.BatchNorm0(inputs)
@@ -131,3 +150,66 @@ class TemporalConv(tf.keras.layers.Layer):
 
     def compute_output_shape(self, input_shape):
         return self.Dropout.compute_output_shape(input_shape)
+
+
+class STConv(tf.keras.layers.Layer):
+    """Basic spatio-temporal convolutional layer"""
+    def __init__(self, filters, kernels=(3, 3), strides=(1, 1), batchnorm=True,
+                 dropout=0.5):
+        """Basic wrapper of ST-GCN
+
+        :param filters: number of output channels
+        :param kernels: a tuple with two elements,
+                        [kernels of temporal-conv, kernels of graphic-conv]
+        :param strides: a tuple with two elements,
+                        [strides of temporal-conv, strides of graphic-conv]
+        :param batchnorm: whether use batchnorm on input and output
+        :param dropout: whether use batchnorm on output
+        """
+        super(STConv, self).__init__()
+        self.filters = filters
+        self.t_kernels, self.s_kernels = kernels
+        self.t_strides, self.s_strides = strides
+        self.batchnorm = batchnorm
+        self.dropout = dropout
+        self.Conv0 = None
+        self.Conv1 = None
+
+    def build(self, input_shape):
+        try:
+            x_shape, A_shape = input_shape
+        except ValueError:
+            raise ValueError("Expected input_shape are [x.shape, A.shape]!")
+
+        if len(x_shape) != 4:
+            raise ValueError("Input x shape should be 3 dimension in "
+                             "[timesteps, nodes, channels], but find "
+                             "{} of {}".format(len(x_shape), x_shape[1:]))
+        if len(A_shape) != 3:
+            raise ValueError("Input A shape should be 3 dimension in "
+                             "[label_number, nodes, nodes], but find "
+                             "{} of {}".format(len(A_shape), A_shape))
+        _, T, N, C = x_shape
+        self.Conv0 = GraphConv(self.filters,
+                                    kernels=self.s_kernels,
+                                    strides=self.s_strides)
+        self.Conv1 = TempoConv(self.filters,
+                               kernels=self.t_kernels,
+                               strides=self.t_strides,
+                               in_batchnorm=self.batchnorm,
+                               out_batchnorm=self.batchnorm,
+                               dropout=self.dropout)
+        super(STConv, self).build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        try:
+            x, A = inputs
+        except ValueError:
+            raise ValueError("Expected inputs are [x, A]!")
+        A = check_adjacent(A)
+        x, A = self.Conv0([x, A])
+        x = self.Conv1(x)
+        return x, A
+
+    def compute_output_shape(self, input_shape):
+        return self.Conv1.compute_output_shape()
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import numpy as np
 import tensorflow as tf
+import tensorflow.keras.backend as K
 from tensorflow.keras.layers import Input
-from lib.gcn import GraphConv, TemporalConv
+from tensorflow.keras.models import Model
+from lib.gcn import GraphConv, TempoConv, STConv
 from lib.graph import Graph
-
 skeleton = Graph("sbu", "spatial")
 
 input_features = Input([30, skeleton.num_node, 3], dtype="float32")
-x = tf.keras.layers.Conv2D(64 * 3, (3, 1), padding="same")(input_features)
-input_A = Input(tensor=tf.keras.backend.constant(skeleton.A))
-x, A = GraphConv(64, t_kernels=3)([input_features, input_A])
-x = TemporalConv(64, dropout=0.5)(x)
-x, A = GraphConv(128, t_kernels=3)([x, A])
-x = TemporalConv(128, dropout=0.5)(x)
-print(x.shape)
+input_A = Input(tensor=K.constant(skeleton.A))
+x, A = STConv(64, kernels=(3, 3), strides=(1, 1))([input_features, input_A])
+x, A = STConv(64, kernels=(3, 3), strides=(1, 1))([x, A])
+x, A = STConv(64, kernels=(3, 3), strides=(1, 1))([x, A])
+x, A = STConv(64, kernels=(3, 3), strides=(1, 1))([x, A])
+x, A = STConv(128, kernels=(3, 3), strides=(2, 1))([x, A])
+x, A = STConv(128, kernels=(3, 3), strides=(1, 1))([x, A])
+x, A = STConv(128, kernels=(3, 3), strides=(1, 1))([x, A])
+x, A = STConv(256, kernels=(3, 3), strides=(2, 1))([x, A])
+x, A = STConv(256, kernels=(3, 3), strides=(1, 1))([x, A])
+output_features, A = STConv(256, kernels=(3, 3), strides=(1, 1))([x, A])
+
+model = Model(inputs=[input_features, input_A], outputs=output_features)
+model.summary()
